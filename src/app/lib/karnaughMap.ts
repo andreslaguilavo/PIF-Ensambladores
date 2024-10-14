f9b398dabf78bfad27c60273fde18be7517141a5
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-10-14 18:44:40 -0500

    Add func getReduceExpression and take out referenceTablePositions var from inside func

--- src/app/lib/karnaughMap.ts
+++ src/app/lib/karnaughMap.ts
@@ -1,3 +1,5 @@
+import QuineMcCluskey from '@helander/quine-mccluskey-js'
+
 type CellsKarMap = number | boolean
 interface HeadersKarMap {
   cols: string[]
@@ -18,26 +20,27 @@ export const referenceTableHeaders: Record<number, HeadersKarMap> = {
   }
 }
 
+const referenceTablePositions: Record<number, CellsKarMap[][]> = {
+  2: [
+    [0, 1],
+    [3, 2]
+  ],
+  3: [
+    [0, 1, 3, 2],
+    [4, 5, 7, 6]
+  ],
+  4: [
+    [0, 1, 3, 2],
+    [4, 5, 7, 6],
+    [12, 13, 15, 14],
+    [8, 9, 11, 10]
+  ]
+}
+
 export const generateKarnaughMap = (
   variables: string[],
   values: CellsKarMap[]
 ) => {
-  const referenceTablePositions: Record<number, CellsKarMap[][]> = {
-    2: [
-      [0, 1],
-      [3, 2]
-    ],
-    3: [
-      [0, 1, 3, 2],
-      [4, 5, 7, 6]
-    ],
-    4: [
-      [0, 1, 3, 2],
-      [4, 5, 7, 6],
-      [12, 13, 15, 14],
-      [8, 9, 11, 10]
-    ]
-  }
   const countVariables = variables.length
   // Calcular el número de filas y columnas de la tabla, basado en el número de variables
   // const countExpCols = Math.ceil(countVariables / 2);
@@ -45,8 +48,10 @@ export const generateKarnaughMap = (
   // const numRows = Math.pow(2, countExpRows);
   // const numCols = Math.pow(2, countExpCols);
 
-  const structureTable = [...referenceTablePositions[countVariables]]
-  for (const row of structureTable) {
+  const deepCopyOfStructureTable = JSON.parse(
+    JSON.stringify(referenceTablePositions[countVariables])
+  ) as CellsKarMap[][]
+  for (const row of deepCopyOfStructureTable) {
     for (const [indexCell, cell] of row.entries()) {
       row[indexCell] = Number(
         values.find((_, index) => {
@@ -56,7 +61,7 @@ export const generateKarnaughMap = (
     }
   }
 
-  return structureTable
+  return deepCopyOfStructureTable
 }
 
 export const getHeadersKarMap = (variables: string[]) => {
@@ -67,4 +72,41 @@ export const getHeadersKarMap = (variables: string[]) => {
   const secondHalf = variables.slice(midIndex) // Desde el medio hasta el final
 
   return [firstHalf, secondHalf]
+}
+
+/**
+ * Reduces a boolean expression using the Quine-McCluskey algorithm and the Maxterms.
+ *
+ * @param variables - An array of variable names used in the boolean expression.
+ * @param values - An array of indexes where a value is 1.
+ * @returns A string representing the minimized boolean expression.
+ */
+export const getReduceExpression = (
+  variables: string[],
+  values: CellsKarMap[][]
+) => {
+  const countVariables = variables.length
+  const indexesWithZero: number[] = []
+
+  for (let index = 0; index < values.length; index++) {
+    const row = values[index]
+    for (let j = 0; j < row.length; j++) {
+      const value = row[j]
+      if (value !== 1) {
+        indexesWithZero.push(
+          Number(referenceTablePositions[countVariables][index][j])
+        )
+      }
+    }
+  }
+  const f = new QuineMcCluskey(variables.join(''), indexesWithZero, [], true)
+
+  const expression = f.getFunction()
+  console.log(expression)
+  const reducedExpression = expression
+    .replace(/\bNOT\s+/g, '!')
+    .replace(/\bAND\b/g, '')
+    .replace(/\bOR\b/g, '+')
+
+  return reducedExpression
 }
